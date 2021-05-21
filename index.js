1645312cba37b784d902da4e682c1a03c18f16e9
AniketS312 <aniketsharma312@gmail.com>
2021-05-20 22:32:43 -0600

    Need to complete accounts and account info section

---
+++ index.js
@@ -0,0 +1,84 @@
+// Initialize array and post it into the transaction section
+const transactions = [
+  {
+    date: "Today",
+    place: "Starbucks",
+    charge: "Card Payment",
+    type: "food",
+    amount: "12.95",
+  },
+  {
+    date: "Today",
+    place: "CostCo Gas",
+    charge: "Card Payment",
+    type: "Automobile",
+    amount: "55.00",
+  },
+  {
+    date: "Today",
+    place: "CostCo Gas",
+    charge: "Card Payment",
+    type: "Automobile",
+    amount: "55.00",
+  },
+  {
+    date: "Today",
+    place: "CostCo Gas",
+    charge: "Card Payment",
+    type: "Automobile",
+    amount: "55.00",
+  },
+  {
+    date: "Today",
+    place: "CostCo Gas",
+    charge: "Card Payment",
+    type: "Automobile",
+    amount: "55.00",
+  },
+  {
+    date: "Today",
+    place: "CostCo Gas",
+    charge: "Card Payment",
+    type: "Automobile",
+    amount: "55.00",
+  },
+  {
+    date: "Today",
+    place: "CostCo Gas",
+    charge: "Card Payment",
+    type: "Automobile",
+    amount: "55.00",
+  },
+  {
+    date: "Today",
+    place: "CostCo Gas",
+    charge: "Card Payment",
+    type: "Automobile",
+    amount: "55.00",
+  },
+  {
+    date: "Today",
+    place: "CostCo Gas",
+    charge: "Card Payment",
+    type: "Automobile",
+    amount: "55.00",
+  },
+  {
+    date: "Today",
+    place: "CostCo Gas",
+    charge: "Card Payment",
+    type: "Automobile",
+    amount: "55.00",
+  },
+  {
+    date: "Today",
+    place: "CostCo Gas",
+    charge: "Card Payment",
+    type: "Automobile",
+    amount: "55.00",
+  },
+];
+
+function postTransactions() {
+
+};
